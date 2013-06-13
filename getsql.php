94cef5872e3a91b7da3fc8e93a80eca5258d4363
MarionGeorges <marion.georges@yahoo.fr>
2013-06-13 21:51:07 +0200

    Ajout de l'appel des event par années

--- getsql.php
+++ getsql.php
@@ -7,10 +7,18 @@ include ('db.php');
 				$sqlget="SELECT * FROM data WHERE annee=".$year." ORDER BY mois ASC";
 				$resget= mysql_query($sqlget);
 
+				$sqleventget="SELECT * FROM event WHERE annee=".$year." ORDER BY mois ASC";
+				$resgetevent=mysql_query($sqleventget);
+				//echo $sqleventget;
+
 echo '[';
 				$i=0;
 				while ($tabget=mysql_fetch_array($resget)){
 
+					while ($tabeventget=mysql_fetch_array($resgetevent)){
+
+						echo $tabeventget['evenement'];
+					}
 
 					if ($i!=0){
 						echo ',';
